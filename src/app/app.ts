4a91c686c70aa3d17d0aaa4c5048ed7e927fae95
jinsoolve <63998352+jinsoolve@users.noreply.github.com>
2025-07-14 00:19:51 +0900

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchComponent } from './components/search/search'; // 경로는 상황에 맞게
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    SearchComponent, // ✅ 검색창 컴포넌트 추가
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]  // ← 이거 추가!
 })
 export class App {
-  protected readonly title = signal('todo-app');
+  // 아무 로직 없어도 됨
 }
